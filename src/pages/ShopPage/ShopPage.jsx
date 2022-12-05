5dc85c785cb840097e1166f0b2e8ffa4e3a31636
Zhamilia <Zhamilia@Jamis-MacBook-Air.local>
2022-12-05 18:59:48 +0600

    fixed bugs 0.5

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -5,7 +5,6 @@ import "./ShopPage.css";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import Category from "../../components/Category/Category";
 import SearchBlock from "../../components/Search/SearchBlock";
-import Category from "../../components/Category/Category.css";
 
 const ShopPage = () => {
   const navigate = useNavigate();
@@ -25,7 +24,7 @@ const ShopPage = () => {
                 &nbsp;&nbsp;&nbsp;—&nbsp;&nbsp;&nbsp;Магазин
               </a>
             </div>
-            <div className="shop-add-product"
+            <div className="shop-add-product">
               <p
                 onClick={() => navigate("/add")}
                 className={` ${user === "admin" ? "see" : "nosee"}`}>
