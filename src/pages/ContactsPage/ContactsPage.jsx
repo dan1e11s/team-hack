1520395c6896cb9ee31803d7128a69f0c357cfd2
Zhamilia <Zhamilia@Jamis-MacBook-Air.local>
2022-12-05 21:43:03 +0600

    map, adapt

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../ContactsPage/ContactsPage.css";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ContactsPage = () => {
   const navigate = useNavigate();
@@ -24,6 +25,19 @@ const ContactsPage = () => {
         </div>
       </div>
       <div className="content_ContactsPage">
+        <div className="map_ContactsPage">
+          <iframe
+            className="map_style_ContactsPage"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2924.058029788211!2d74.58473861546689!3d42.87161931063404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb7d04d040001%3A0x435e5287f35c7d3c!2z0JrRg9GA0YHRiyDQv9GA0L7Qs9GA0LDQvNC80LjRgNC-0LLQsNC90LjRjyBNYWtlcnMgQ29kaW5nIEJvb3RjYW1w!5e0!3m2!1sru!2skg!4v1670175731064!5m2!1sru!2skg"
+            style={{
+              width: "100%",
+              // height: "100%",
+              style: "border:0;",
+              allowfullscreen: "",
+              loading: "lazy",
+              referrerpolicy: "no-referrer-when-downgrade",
+            }}></iframe>
+        </div>
         <div className="contacts-wrapper">
           <div className="rowText">
             <div className="firstColumn">
@@ -55,7 +69,20 @@ const ContactsPage = () => {
                 type="text"
                 placeholder="Сообщение"
               />
-              <button className="btn_ContactsPage">Отправить</button>
+              <button
+                onClick={() => {
+                  Swal.fire({
+                    title: "Успешно отправлено!",
+                    confirmButtonText: "Ok",
+                  }).then(result => {
+                    if (result.isConfirmed) {
+                      navigate("/");
+                    }
+                  });
+                }}
+                className="btn_ContactsPage">
+                Отправить
+              </button>
             </div>
           </div>
         </div>
